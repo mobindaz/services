1dd6b86f123d749844087d8cb42756886257e703
Sudevan <sudevank@gmail.com>
2024-06-30 10:11:12 +0530

    "Remove __pycache__ from tracking"

--- services/settings.py
+++ services/settings.py
@@ -11,7 +11,14 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import environ
+from pathlib import Path
 
+env = environ.Env(
+    # Set casting, default value
+    DEBUG=(bool, False)
+)
+environ.Env.read_env(env_file='.env')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -24,7 +31,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd3kavfz(*0#66o+3*g!aa(7bv!c7$gzi-#)1@h%qwqtjoe%soj'
+#SECRET_KEY = 'd3kavfz(*0#66o+3*g!aa(7bv!c7$gzi-#)1@h%qwqtjoe%soj'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -92,17 +100,20 @@ WSGI_APPLICATION = 'services.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'services',
-        'USER': 'data',
-        'PASSWORD': 'data',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'gservice',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5434',
+#     }
+# }
 
+DATABASES = {
+    'default': env.db(),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
